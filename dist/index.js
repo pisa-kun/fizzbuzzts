5ae7f153d7242932abe90a97de57192ff53e2ce9
pisa <pisa0401mst@gmail.com>
2021-11-08 00:26:07 +0900

    fix index.ts

--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,11 @@ for (var i = 0; i <= 100; i++) {
     var message = (0, fizzbuzz_1.fizzbuzz)(i);
     console.log(i, " is ", message);
 }
+for (var x = 1; x <= 100; x++) {
+    var a = x % 15 == 0 ? "FizzBuzz"
+        : x % 5 == 0 ? "Buzz"
+            : x % 3 == 0 ? "Fizz"
+                : "" + x;
+    console.log(a);
+}
 //# sourceMappingURL=index.js.map
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,6 +2,16 @@ import { fizzbuzz } from "./fizzbuzz/fizzbuzz";
 
 console.log("fizzbuzz問題")
 for(let i = 0; i <= 100; i++){
+    // import先の関数を使用
     let message = fizzbuzz(i)
     console.log(i, " is ", message)
+}
+
+// badcase
+for(let x = 1; x <= 100; x++){
+    let a = x % 15 == 0 ? "FizzBuzz" 
+    : x % 5 == 0 ? "Buzz" 
+    : x % 3 == 0 ? "Fizz" 
+    : `${x}`;
+    console.log(a)
 }
